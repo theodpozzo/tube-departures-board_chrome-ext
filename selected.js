50d54736aacd912b2bb59af9c18a9528a56354fa
theosantos3 <theosantos3@gmail.com>
2023-07-13 14:55:25 +0100

    🎨 Added line colours

--- selected.js
+++ selected.js
@@ -63,6 +63,8 @@ function populateTable(results) {
     results.forEach((result) => {
         const row = document.createElement('tr');
 
+        setColour(row, result);
+
         const line = document.createElement('td');
         line.textContent = result.lineName;
 
@@ -84,4 +86,40 @@ function populateTable(results) {
 
         table.appendChild(row);
     });
+}
+
+function setColour(row, result) {
+    if (result.lineName === 'Circle') {
+        row.style.borderLeft = '5px solid #FFCE00';
+    } else if (result.lineName === 'District') {
+        row.style.borderLeft = '5px solid #007229';
+    } else if (result.lineName === 'Hammersmith & City') {
+        row.style.borderLeft = '5px solid #F3A9BB';
+    } else if (result.lineName === 'Jubilee') {
+        row.style.borderLeft = '5px solid #A0A5A9';
+    } else if (result.lineName === 'Metropolitan') {
+        row.style.borderLeft = '5px solid #9B0056';
+    } else if (result.lineName === 'Northern') {
+        row.style.borderLeft = '5px solid #000000';
+    } else if (result.lineName === 'Piccadilly') {
+        row.style.borderLeft = '5px solid #003688';
+    } else if (result.lineName === 'Victoria') {
+        row.style.borderLeft = '5px solid #0098D4';
+    } else if (result.lineName === 'Waterloo & City') {
+        row.style.borderLeft = '5px solid #95CDBA';
+    } else if (result.lineName === 'Bakerloo') {
+        row.style.borderLeft = '5px solid #B05C2C';
+    } else if (result.lineName === 'Central') {
+        row.style.borderLeft = '5px solid #E32017';
+    } else if (result.lineName === 'DLR') {
+        row.style.borderLeft = '5px solid #00A4A7';
+    } else if (result.lineName === 'London Overground') {
+        row.style.borderLeft = '5px solid #EE7C0E';
+    } else if (result.lineName === 'TfL Rail') {
+        row.style.borderLeft = '5px solid #0019A8';
+    } else if (result.lineName === 'Tram') {
+        row.style.borderLeft = '5px solid #84B817';
+    } else {
+        row.style.borderLeft = '5px solid #000000';
+    }
 }
